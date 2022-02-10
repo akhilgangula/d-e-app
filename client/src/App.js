6680dfddf8fadddf605f0538092e403e0e30d7f0
Akhil Reddy Gangula <akhilreddy.gangula96@gmail.com>
2022-02-09 19:12:22 -0500

    backup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import SimpleStorageContract from "./contracts/MetaCoin.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { sender: 0, receiver: 0, web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
@@ -37,15 +37,17 @@ class App extends Component {
 
   runExample = async () => {
     const { accounts, contract } = this.state;
-
+    const senderAddress = accounts[0];
+    const receiverAddress = "0xdde936058C1f4f6Af78B491205397Da60D753a15";
     // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+    await contract.methods.sendCoin(receiverAddress, 1000).send({ from: senderAddress });
 
     // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
+    const sender = await contract.methods.getBalance(senderAddress).call();
+    const receiver = await contract.methods.getBalance(receiverAddress).call();
 
     // Update state with the result.
-    this.setState({ storageValue: response });
+    this.setState({ sender, receiver });
   };
 
   render() {
@@ -54,17 +56,9 @@ class App extends Component {
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <div>Sender's address: {this.state.sender}</div>
+        <div>Receiver's address: {this.state.receiver}</div>
+        <button onClick={this.runExample}>Send</button>
       </div>
     );
   }
